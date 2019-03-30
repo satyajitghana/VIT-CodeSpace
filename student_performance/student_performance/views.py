d6bf06fe00a4b047d163a2bac7fc931d1f91b0bb
Aryan <aryanjalali1234@gmail.com>
2019-03-30 09:41:14 +0530

    made some changes

--- student_performance/student_performance/views.py
+++ student_performance/student_performance/views.py
@@ -1,9 +1,12 @@
 from flask import render_template
+from flask import request
 
 from student_performance import app
 
 
 @app.route('/')
 def index():
+    var = request.args.get('id')
+    print(var)
     app.logger.warning('sample message')
     return render_template('index.html')
